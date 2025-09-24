6322065a9a1b100528a2d491810af88f424bb879
BLuMuun <marceloantoniogalaztoro@gmail.com>
2025-09-24 01:04:27 -0300

    Proyecto SuperMarket

---
+++ supermarket/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", include("miapp.urls")),  # Redirige a tu app principal
+]
+
+# Soporte para archivos de imágenes (productos)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
